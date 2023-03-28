ffedbcbee51c90630f3b67dc2d453d29c266e59b
Tcc <1481705659@qq.com>
2023-03-28 22:20:02 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,6 +6,6 @@ public class GitTest {
         System.out.println("sffdgf");
         System.out.println("ajkdsvnfd");
         System.out.println("adsvbd");
-        System.out.println("dsvdf");
+        System.out.println("master test");
     }
 }
